c69d311ea2dea0d674461f33e11d2ae9c30b67ea
isxoqovbegzod <91113167+isxoqovbegzod@users.noreply.github.com>
2022-04-28 16:38:51 +0500

    add togo

--- app/newapp/urls.py
+++ app/newapp/urls.py
@@ -3,6 +3,7 @@ from .views import index_page, handler404, vido_template, some_view
 
 # from django.conf.urls import handler404
 
+
 urlpatterns = [
     path('', index_page, name='index'),
     path('video/', vido_template, name='video'),
